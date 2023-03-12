b6eca580bca6043c4402dd0dc260584f17654dd5
Ho, Kum Wah <jinhua7@gmail.com>
2023-03-12 21:54:37 +1000

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Ho Kum Wah on 12/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
